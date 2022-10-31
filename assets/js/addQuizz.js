e5173bf3a8f63fc495d9fc0922585cf3ae437e34
JC4F <lequan12122002@gmail.com>
2022-10-31 17:31:00 +0700

    cpl logic add quiz

--- assets/js/addQuizz.js
+++ assets/js/addQuizz.js
@@ -26,12 +26,12 @@ const arrangeOrderAnswers = () => {
         listAnswers[i].querySelector('.col-form-label span').innerHTML = alphabets[i];
     }
 }
-const handleRemoveClick = (e) => {
+const handleRemoveAnswerClick = (e) => {
     let parentEle = e.srcElement.closest('.add-answer__item')
     parentEle.remove();
     arrangeOrderAnswers();
 }
-const handleAddClick = () => { 
+const handleAddAnswerClick = () => { 
     let nextAnswer = document.createElement('div');
     nextAnswer.className ='add-answer__item';
     nextAnswer.innerHTML = `<div class="mb-3 row">
@@ -51,22 +51,23 @@ const handleAddClick = () => {
 
     let addIcon = nextAnswer.querySelector('.fa-circle-plus')
     let remmoveIcon = nextAnswer.querySelector('.fa-trash-can')
-    remmoveIcon.addEventListener('click', handleRemoveClick)
-    addIcon.addEventListener('click', handleAddClick)
+    remmoveIcon.addEventListener('click', handleRemoveAnswerClick)
+    addIcon.addEventListener('click', handleAddAnswerClick)
     arrangeOrderAnswers();
 }
 const handleAddNewAnswer = () => {
     let addBtns = document.querySelectorAll('.aai-action__wrapper .fa-circle-plus')
     let removeBtns = document.querySelectorAll('.aai-action__wrapper .fa-trash-can')
     for(let addBtn of addBtns){
-        addBtn.addEventListener('click', handleAddClick)
+        addBtn.addEventListener('click', handleAddAnswerClick)
     }
     for(let removeBtn of removeBtns){
-        removeBtn.addEventListener('click', handleRemoveClick)
+        removeBtn.addEventListener('click', handleRemoveAnswerClick)
     }
 }
 
 const handleAddNextQuestion = () => {
+    _$('.add-question__heading button').innerHTML = 'Add to Quizz'
     _$('.add-question__index').innerHTML = listQues.length + 1;
     _$('.add-question__content textarea').value = '';
     let answerList = document.querySelectorAll('.add-answer__item')
@@ -84,17 +85,26 @@ const handleAddNewQuestion = () => {
     addBtn.addEventListener('click', handleAddNextQuestion)
 }
 
+const handleClearOtherWhenAddQuizz = ()=>{
+    let allInputChecks = document.querySelectorAll('input[type="checkbox"]')
+    for(let allInputCheck of allInputChecks){
+        allInputCheck.checked = false;
+    }
+}
 const renderItemToListQuiz = () => {
     let nextItem = document.createElement('div');
     nextItem.className ='quizz-wrapper__item';
     nextItem.innerHTML = `<div class="qw-item__left">
             <input class="form-check-input mx-1" type="checkbox">
-            <span>Exercise ${listQues.length}</span>
+            <span>Exercise <span class="qw-item__index">${listQues.length}</span></span>
         </div>
         <div class="qw-item__right">
             <i class="fa-solid fa-trash-can"></i>
         </div>`;
     _$('.quizz-wrapper__list').appendChild(nextItem)
+    nextItem.querySelector('.fa-trash-can').addEventListener('click', handleClickDelete)
+    nextItem.querySelector('input').addEventListener('click', handleVerifyCheckAll)
+    nextItem.querySelector('input + span').addEventListener('click', handleClickSelect)
 }
 const handleAddQuiz = () => {
     let addToQuizzBtn = _$('.add-question__heading button');
@@ -102,20 +112,143 @@ const handleAddQuiz = () => {
         let answerList = document.querySelectorAll('.add-answer__item')
         let question = _$('.add-question__content textarea').value;
         let ansList = [...answerList].map((cur, index)=>{
-            return {
-                answer: cur.querySelector('input[type="text"]').value, 
-                istrue: cur.querySelector('input[type="checkbox"]').checked
-            }
+                return {
+                    answer: cur.querySelector('input[type="text"]').value, 
+                    istrue: cur.querySelector('input[type="checkbox"]').checked
+                }
         })
-        let singleQues = {
-            question, ansList
+        if(addToQuizzBtn.innerHTML=='Add to Quizz'){
+            let singleQues = {
+                question, ansList
+            }
+            listQues.push(singleQues)
+            renderItemToListQuiz()
+            handleAddNextQuestion()
+            handleClearOtherWhenAddQuizz();
+        } else if(addToQuizzBtn.innerHTML=='Update Quizz'){
+            let index = +_$('.add-question__index').innerHTML-1;
+            listQues[index] = {...listQues[index], question, ansList}
         }
-        listQues.push(singleQues)
-        renderItemToListQuiz()
     })
 }
 
-const handleAddQuizz = ()=>{
+
+const ArrangeNumQuizz = ()=>{
+    let quizzItemIndexs = document.querySelectorAll('.qw-item__index')
+    for(let i=0; i<quizzItemIndexs.length; i++){
+        quizzItemIndexs[i].innerHTML = i+1;
+    }
+}
+const handleClickDelete = (e)=>{
+    let removeEle = e.srcElement.closest('.quizz-wrapper__item');
+    listQues.splice(+removeEle.querySelector('.qw-item__index').innerHTML-1, 1)
+    removeEle.remove()
+    ArrangeNumQuizz();
+    _$('.add-question__index').innerHTML = listQues.length + 1;
+}
+const handleDeleletSeleted = ()=>{
+    let selectedDelBtn = _$('.quizz-wrapper__heading .btn-danger')
+    selectedDelBtn.addEventListener('click', ()=>{
+        let checkItems = document.querySelectorAll('.quizz-wrapper__item input')
+        for(let checkItem of checkItems){
+            if(checkItem.checked){
+                listQues.splice(+checkItem.closest('.quizz-wrapper__item').querySelector('.qw-item__index').innerHTML-1, 1)
+                checkItem.closest('.quizz-wrapper__item').remove()
+                ArrangeNumQuizz();
+            }
+        }
+        _$('.add-question__index').innerHTML = listQues.length + 1;
+    })
+}
+const handleDeleteListQuizz = () => {
+    let listQuizzes = document.querySelectorAll('.quizz-wrapper__item .fa-trash-can')
+    if(listQuizzes){
+        for(let listQuizz of listQuizzes){
+            listQuizz.addEventListener('click', handleClickDelete)
+        }
+    }
+}
+
+const handleVerifyCheckAll = ()=>{
+    let checkAll = _$('.quizz-wrapper__heading input')
+    let checkList = document.querySelectorAll('.quizz-wrapper__item input')
+    for(let checkItem of checkList){
+        if(!checkItem.checked) {
+            checkAll.checked = false;
+            return;
+        }
+    }
+    checkAll.checked = true;
+}
+const handleCheckQuiz = ()=>{
+    let checkAll = _$('.quizz-wrapper__heading input')
+    let checkList = document.querySelectorAll('.quizz-wrapper__item input')
+    for(let checkItem of checkList){
+        checkItem.addEventListener('click', handleVerifyCheckAll)
+    }
+    checkAll.addEventListener('click', ()=>{
+        let checkListTmp = document.querySelectorAll('.quizz-wrapper__item input')
+        for(let checkItem of checkListTmp){
+            checkItem.checked = checkAll.checked;
+        }
+    })
+}
+
+const handleClickSelect = (e)=>{
+    handleAddNextQuestion();
+    let clickSelect = e.srcElement;
+    let index = +clickSelect.querySelector('.qw-item__index').innerHTML;
+    _$('.add-question__index').innerHTML = index;
+    _$('.add-question__content textarea').value = listQues[index-1].question;
+    
+    //answer
+    document.querySelectorAll('.add-answer__item input[type="checkbox"]')[0].checked = listQues[index-1].ansList[0].istrue
+    document.querySelectorAll('.add-answer__item input[type="text"]')[0].value = listQues[index-1].ansList[0].answer
+
+    listQues[index-1].ansList.forEach((cur, index1)=>{
+        if(index1!=0){
+            let nextAnswer = document.createElement('div');
+            nextAnswer.className ='add-answer__item';
+            nextAnswer.innerHTML = `<div class="mb-3 row">
+                    <label class="col-sm-1 col-form-label">
+                        <input class="form-check-input mx-1" type="checkbox" ${cur.istrue?'checked':''}>
+                        <span>${alphabets[index1]}</span>
+                    </label>
+                    <div class="col-sm-8">
+                    <input type="text" class="form-control" value="${cur.answer}">
+                    </div>
+                    <div class="col-sm-2 aai-action__wrapper">
+                        <i class="fa-solid fa-circle-plus"></i>
+                        <i class="fa-solid fa-trash-can"></i>
+                    </div>
+                </div>`;
+            _$('.add-answer__List').appendChild(nextAnswer)
+    
+            let addIcon = nextAnswer.querySelector('.fa-circle-plus')
+            let remmoveIcon = nextAnswer.querySelector('.fa-trash-can')
+            remmoveIcon.addEventListener('click', handleRemoveAnswerClick)
+            addIcon.addEventListener('click', handleAddAnswerClick)
+        }
+    })
+    _$('.add-question__heading button').innerHTML = 'Update Quizz'
+}
+const handleSelectQuestion = ()=>{
+    let selectQuestions = document.querySelectorAll('.qw-item__left input + span')
+    if(selectQuestions){
+        for(let selectQuestion of selectQuestions){
+            selectQuestion.addEventListeners('click', handleClickSelect)
+        }
+    }
+}
+
+const handleSubmitAddQuizz = () => {
+    let submitBtn = _$('.header-control .btn-success')
+    submitBtn.addEventListener('click', () =>{
+        console.log(listQues)
+    })
+}
+
+const handleAddQuizzToSystem = ()=>{
     handleImages();
     let allData = {}
     let quizzInfo = {
@@ -125,11 +258,15 @@ const handleAddQuizz = ()=>{
         code: "",
         time: ""
     };
-    
 
     handleAddNewAnswer()
-    handleAddQuiz()
     handleAddNewQuestion()
+    handleAddQuiz()
+    handleDeleteListQuizz()
+    handleCheckQuiz()
+    handleDeleletSeleted()
+    handleSelectQuestion()
+    handleSubmitAddQuizz()
 }
 
-handleAddQuizz()
+handleAddQuizzToSystem()
